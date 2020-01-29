92b42efa76da7f0faea2038e067483e40e50419b
Laurie Barth <laurie@LauriesrkLaptop.fios-router.home>
2020-01-29 10:09:56 -0500

    Blog and notes theme clash

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Testing Out Themes`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `Laurie B`,
+    social: [
+      {name: `Twitter`,
+    url: `https://twitter.com/gatsbyjs`}
+    ]
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -25,6 +29,27 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {
+        /*
+        - basePath defaults to `/`
+        - contentPath defaults to `content/posts`
+        - assetPath defaults to `content/assets`
+        - mdx defaults to `true`
+        */
+        basePath: `/blog`,
+        contentPath: `content/blogPosts`,
+        assetPath: `content/blogAssets`,
+      },
+    },
+    {
+      resolve: `gatsby-theme-notes`,
+      options: {
+        // basePath defaults to `/`
+        basePath: `/notes`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
